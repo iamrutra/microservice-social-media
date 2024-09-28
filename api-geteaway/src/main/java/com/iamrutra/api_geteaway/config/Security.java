99a738a30c85da46d4b39974100233eb332fec92
iamrutra <iamrutra@gmail.com>
2024-09-29 00:09:18 +0200

    fixed an api-gateway and added keycloak

--- api-geteaway/src/main/java/com/iamrutra/api_geteaway/config/Security.java
+++ api-geteaway/src/main/java/com/iamrutra/api_geteaway/config/Security.java
@@ -6,13 +6,9 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.cors.CorsConfigurationSource;
 
 import java.util.List;
 
@@ -33,14 +29,9 @@ public class Security {
         return http.build();
     }
 
-    @Bean
-    public JwtDecoder jwtDecoder() {
-        // Use your JWT issuer URL here
-        return NimbusJwtDecoder.withJwkSetUri("http://localhost:8080/realms/iamrutra/protocol/openid-connect/certs").build();
-    }
 
     @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
+    public UrlBasedCorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("http://localhost:3000"));  // Frontend URL
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
